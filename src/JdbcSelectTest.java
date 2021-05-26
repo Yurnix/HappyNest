a8a568ac087e2eb2ffe521a54afc227ad9cef86f
Yunix <Yunix@Yunix>
2021-05-26 23:35:16 +0300

    Online jdbc

--- src/JdbcSelectTest.java
+++ src/JdbcSelectTest.java
@@ -12,14 +12,38 @@ public class JdbcSelectTest {   // Save as "JdbcSelectTest.java"
             String a;
             a = "made some changes";
                                      // The format is: "jdbc:mysql://hostname:port/databaseName", "username", "password"
-            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbHappyNest?", "root", "ebisonovsekonem");
-            Statement stmt = c.createStatement();
+            //Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbHappyNest?", "root", "ebisonovsekonem");
+            
+            Connection cloud = DriverManager.getConnection(
+                    "jdbc:mysql://fdb28.awardspace.net:3306/3783121_dbhappynest",
+                    "3783121_dbhappynest",
+                    "Ebisonovsekonem1!");
+            
+            
+            //Statement stmt = c.createStatement();
+            Statement c_stmt = cloud.createStatement();
+            
             String command;
+            command = "insert into products values ("
+                    + "1,"
+                    + "Coca Cola,"
+                    + "\"En mia malakia j misi \n 8=D\" , "
+                    + "1919199191,"
+                    + "10,"
+                    + "0.3,"
+                    + "13,"
+                    + "1,"
+                    + "1)";
+            
+       
+            
+            
             ResultSet result;
-            command = "select * from suppliers";
-
-            result = stmt.executeQuery(command);
-
+            
+            result = c_stmt.executeQuery(command);
+            System.out.print("After execution : " + result.toString());
+            command = "select productName from suppliers";
+            result = c_stmt.executeQuery(command);
             /* 
             create table suppliers (
             supplierID int,
@@ -30,18 +54,19 @@ public class JdbcSelectTest {   // Save as "JdbcSelectTest.java"
              */
             
             while (result.next()) {
-                System.out.println(
+                System.out.println(result.getString("productName"));
+                /*System.out.println(
                         result.getInt("supplierID")
                         + " | "
                         + result.getString("supplierName")
                         + " | "
                         + result.getString("supplierDesc")
                         + " | "
-                        + result.getLong("supplierEntryDate"));
+                        + result.getLong("supplierEntryDate"));*/
             }
 
         } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
+            System.out.println("Exception: " + ex.getMessage());
         }
     }
 }
