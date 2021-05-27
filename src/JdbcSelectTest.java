e110a8e2762b0507c5b0e7875721635200b3f060
Yunix <Yunix@Yunix>
2021-05-28 00:11:18 +0300

    sql works

--- src/JdbcSelectTest.java
+++ src/JdbcSelectTest.java
@@ -1,11 +1,9 @@
 
-import java.sql.*;  // Using 'Connection', 'Statement' and 'ResultSet' classes in java.sql package
+import java.sql.*;  
 import java.util.logging.Level;
 import java.util.logging.Logger;
-//some changes
 
-//EDO EGRAPSE O POUYAS
-public class JdbcSelectTest {   // Save as "JdbcSelectTest.java"
+public class JdbcSelectTest {   
 
     public static void main(String[] args) {
         try {
@@ -14,16 +12,27 @@ public class JdbcSelectTest {   // Save as "JdbcSelectTest.java"
                                      // The format is: "jdbc:mysql://hostname:port/databaseName", "username", "password"
             //Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbHappyNest?", "root", "ebisonovsekonem");
             
+            /*
             Connection cloud = DriverManager.getConnection(
                     "jdbc:mysql://fdb28.awardspace.net:3306/3783121_dbhappynest",
                     "3783121_dbhappynest",
-                    "Ebisonovsekonem1!");
+                    "Ebisonovsekonem1!"
+            );
+            */
+            
+            Connection cloud = DriverManager.getConnection(
+                    "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11415364",
+                    "sql11415364",
+                    "fWjVyJaAt9"
+            );
             
             
             //Statement stmt = c.createStatement();
+            
             Statement c_stmt = cloud.createStatement();
             
             String command;
+            /*
             command = "insert into products values ("
                     + "1,"
                     + "Coca Cola,"
@@ -34,15 +43,19 @@ public class JdbcSelectTest {   // Save as "JdbcSelectTest.java"
                     + "13,"
                     + "1,"
                     + "1)";
+            */
             
-       
-            
-            
+            //supplierID 	supplierName 	supplierDesc 	supplierEntryDate 
+            command = "insert into suppliers values ("
+                    + "1,"
+                    + "\"Yuri\","
+                    + "\"The desc works\","
+                    + "20002929221)";
+            System.out.println(command);
             ResultSet result;
-            
-            result = c_stmt.executeQuery(command);
-            System.out.print("After execution : " + result.toString());
-            command = "select productName from suppliers";
+            c_stmt.executeUpdate(command);
+            //System.out.print("After execution : " + result.toString());
+            command = "select supplierName from suppliers";
             result = c_stmt.executeQuery(command);
             /* 
             create table suppliers (
@@ -54,7 +67,7 @@ public class JdbcSelectTest {   // Save as "JdbcSelectTest.java"
              */
             
             while (result.next()) {
-                System.out.println(result.getString("productName"));
+                System.out.println(result.getString("supplierName"));
                 /*System.out.println(
                         result.getInt("supplierID")
                         + " | "
